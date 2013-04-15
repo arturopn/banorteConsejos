e41c62f51a09705b73cbd85c3255b5f1de415386
Arturo Pérez Navarro <arturo.pereznavarro87@gmail.com>
2013-04-15 11:26:48 -0500

    alta
    se dio de alta el nuevo repositorio

---
+++ assets/js/engine.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+	$('div.fotobg').hide();
+
+	//fix html5 placeholder attribute for ie7 & ie8
+	if (jQuery.browser.msie && jQuery.browser.version.substr(0,1) < 9) { // ie7&ie8
+		jQuery('input[placeholder], textarea[placeholder]').each(function(){          
+			var input = $(this);        
+			
+			$(input).val(input.attr('placeholder'));
+							
+			$(input).focus(function(){
+				if (input.val() == input.attr('placeholder')) {
+					input.val('');
+				}
+			});
+			
+			$(input).blur(function(){
+				if (input.val() == '' || input.val() == input.attr('placeholder')) {
+					input.val(input.attr('placeholder'));
+				}
+			});
+		});
+	}
+
+
+$('#new_user').submit(function(event){
+    $('.required').each(function(){
+        if(!$.trim($(this).val()))
+        {
+            $(this).addClass('emptyField');
+            event.preventDefault();
+        }
+    });
+});
+
+
+$('.fotologin').click({
+   $('div.fotobg').show(400);
+});
+
+});
